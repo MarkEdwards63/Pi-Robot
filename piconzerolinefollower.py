6e115f94e4d9760571b1f8044c872b1b7eb1e405
MarkEdwards63 <mark@theedwardsfamily.com>
2017-01-29 12:51:24 +0000

    Add files via upload

--- piconzerolinefollower.py
+++ piconzerolinefollower.py
@@ -18,6 +18,7 @@ middlePin = 27
 rightPin = 22
 
 # Setup pins for line following sensor
+GPIO.setwarnings(False)         # disable warnings
 GPIO.setmode(GPIO.BCM)          # Broadcom pin-numbering scheme
 GPIO.setup(leftPin, GPIO.IN)    # set pins as inputs for GPIO
 GPIO.setup(middlePin, GPIO.IN)
@@ -32,7 +33,7 @@ pz.init()
 #reducepower = 1.0
 
 # Setup the power limit
-maxpower = 60
+maxPower = 60
 
 # make sure motors are off 
 pz.stop()
@@ -46,15 +47,15 @@ def readInputs():
     right = 0
 
     if GPIO.input(leftPin) == False:
-        print("left\n")
+        print("left")
         left = 1
 
     if GPIO.input(middlePin) == False:
-        print("middle\n")
+        print("middle")
         middle = 1
 
     if GPIO.input(rightPin) == False:
-        print("right\n")
+        print("right")
         right = 1
 
     returnValues = [left, middle, right]
@@ -76,37 +77,45 @@ try:
         print(line)
         time.sleep(interval) # wait between readings
 	
-        # if line is on left turn right
+        # if line is on right turn rigftleht
         if (line == [0, 1, 1]) or (line == [0, 0, 1]):
-            driveLeft = -0.6
-            driveRight = 0.6
-		 
+            driveLeft = 0.6
+            driveRight = -0.6
+            print("Turn Left")
+            
         # if line is central, power both motors
         if (line == [1, 0, 1]):
             driveLeft = 1
             driveRight = 1
+            print("Straight")
 		 
-        # if line is on right turn left
+        # if line is on left turn right
         if (line == [1, 1, 0]) or (line == [1, 0, 0]):
-            driveLeft = 0.6
-            driveRight =-0.6
+            driveLeft = -0.6
+            driveRight = 0.6
+            print("Turn Right")
 
         # if can't see line, repeat last move
         if (line == [1, 1, 1]):
-            driveLeft = oldDriveLeft
-            driveRight = oldDriveRight
-
+#            driveLeft = oldDriveLeft
+#            driveRight = oldDriveRight
+#            print("Repeat")
+             driveLeft = 1
+             driveRight = 1
+             print("Straight 1 1 1 ")
+            
         # if can't see line, repeat last move
         if (line == [0, 0, 0]):
             driveLeft = oldDriveLeft
             driveRight = oldDriveRight
+            print("rEPEAT ")
         
         # save driveLeft and driveRight to oldDriveLeft and oldDriveRight
         oldDriveLeft = driveLeft
         oldDriveRight = driveRight
 	
         # update motor values
-        print(maxPower, driveLeft, driveRight)	
+        print("Power / Left / Right", maxPower, driveLeft, driveRight)	
         pz.setMotor(leftMotor, int(-driveLeft * maxPower))
         pz.setMotor(rightMotor, int(-driveRight * maxPower))
 
@@ -115,4 +124,5 @@ except KeyboardInterrupt:
     print ("Keyboard Interupt")
     
 finally:
+    print("Cleanup at end")
     pz.cleanup()
